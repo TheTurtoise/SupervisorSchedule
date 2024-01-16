656f69ddd8faa882438412ba27ea997a27b3d0de
Arjun Chelliah <53024410+coolboy56744@users.noreply.github.com>
2024-01-16 13:31:15 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,22 +3,33 @@ import certExamPractice.FileIO;
 import java.util.HashMap;
 
 public class Main {
-    static HashMap<Integer, Integer> sizeList = new HashMap<>();
+    static HashMap<String, Integer> sizeList = new HashMap<>();
 
     public static void main(String[] args) {
+
+        FileIO.instantiate();
+
+        // stored as (row #, column #) (y, x)
+        FileIO.reading();
+
+        System.out.println(FileIO.getDataTable().get(0).get(1));
+
+        for (int i = 0; i < FileIO.getDataTable().size(); i++) {
+            System.out.println(FileIO.getDataTable().get(i));
+        }
+
         // File IO for staff name list
         // staff names would replace "i" (key)
         // 0 represents tally of total supervisions (value)
-        for (int i = 0; i < 120; i++) {
-            sizeList.put(i, 0);
-        }
-
-        FileIO.reading();
-
-//        FileIO.writing();
+//        for (int i = 0; i < FileIO.getDataTable().size(); i++) {
+//            System.out.println(i);
+//            sizeList.put(FileIO.getDataTable().get(i).get(2), 0);
+//        }
 
         System.out.println(sizeList);
 
-        new CalendarGUI();
+//        new CalendarGUI();
+
+//        FileIO.writing();
     }
 }
