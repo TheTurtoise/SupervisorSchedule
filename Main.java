8bd61358876453236821c4b1a253150f0c5839bc
Arjun Chelliah <53024410+coolboy56744@users.noreply.github.com>
2024-01-19 14:54:57 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,29 +6,21 @@ public class Main {
     static HashMap<String, Integer> sizeList = new HashMap<>();
 
     public static void main(String[] args) {
-
-        FileIO.instantiate();
-
         // stored as (row #, column #) (y, x)
         FileIO.reading();
-
-        System.out.println(FileIO.getDataTable().get(0).get(1));
-
-        for (int i = 0; i < FileIO.getDataTable().size(); i++) {
-            System.out.println(FileIO.getDataTable().get(i));
-        }
 
         // File IO for staff name list
         // staff names would replace "i" (key)
         // 0 represents tally of total supervisions (value)
-//        for (int i = 0; i < FileIO.getDataTable().size(); i++) {
-//            System.out.println(i);
-//            sizeList.put(FileIO.getDataTable().get(i).get(2), 0);
-//        }
+        for (int i = 0; i < FileIO.getDataTable().size(); i++) {
+            sizeList.put(FileIO.getDataTable().get(i).get(2), 0);
+        }
 
-        System.out.println(sizeList);
+//        System.out.println(sizeList);
 
-//        new CalendarGUI();
+        new ScheduleGUI();
+
+
 
 //        FileIO.writing();
     }
