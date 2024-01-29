e29310ebff567ea40309b4788508563a148a177b
TheTurtoise <151710601+TheTurtoise@users.noreply.github.com>
2024-01-28 20:51:12 -0500

    Update TallyChart.java

--- TallyChart.java
+++ TallyChart.java
@@ -13,14 +13,14 @@ public class TallyChart {
         model.addColumn("Staff Name");
         model.addColumn("Number of Shifts");
         model.addColumn("Availability");
-        
+
         for (int i = 1; i <= 120; i++) {
             model.addRow(new Object[]{" ", i, "Available/Unavailable"});
         }
 
         JTable staffChartTable = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(staffChartTable);
-
+        staffChartTable.setRowHeight(20);
         frame.add(scrollPane, BorderLayout.CENTER);
         frame.setSize(600, 400);
         frame.setVisible(true);
