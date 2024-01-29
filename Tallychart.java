1f2fa301c86a79ab088f249f817e2dd845e66947
TheTurtoise <151710601+TheTurtoise@users.noreply.github.com>
2024-01-28 20:47:33 -0500

    Update Tallychart.java

--- Tallychart.java
+++ Tallychart.java
@@ -1,34 +1,28 @@
-// TallyChart.java
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
-public class TallyChart extends JFrame {
-    public TallyChart(String[] teacherNames) {
-        setTitle("Staff Chart");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+public class TallyChart {
 
-        // these are the columns
+    private JFrame frame;
+
+    public TallyChart() {
+        frame = new JFrame("Staff Chart");
 
         DefaultTableModel model = new DefaultTableModel();
         model.addColumn("Staff Name");
         model.addColumn("Number of Shifts");
         model.addColumn("Availability");
-
-        //amount of teachers, can add more or less here
         
-        for (String teacherName : teacherNames) {
-            for (int i = 1; i <= 120; i++) {
-                model.addRow(new Object[]{" ", i, "Available/Unavailable"});
-            }
+        for (int i = 1; i <= 120; i++) {
+            model.addRow(new Object[]{" ", i, "Available/Unavailable"});
         }
 
         JTable staffChartTable = new JTable(model);
         JScrollPane scrollPane = new JScrollPane(staffChartTable);
 
-        add(scrollPane, BorderLayout.CENTER);
-        setSize(600, 400);
-        setVisible(true);
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
     }
 }
-
