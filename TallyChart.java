5359fee9e84253999ac41f7470f6032e376cdf89
TheTurtoise <151710601+TheTurtoise@users.noreply.github.com>
2024-01-28 20:46:11 -0500

    Create TallyChart.java

---
+++ TallyChart.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+
+public class TallyChart {
+
+    private JFrame frame;
+
+    public TallyChart() {
+        frame = new JFrame("Staff Chart");
+
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Staff Name");
+        model.addColumn("Number of Shifts");
+        model.addColumn("Availability");
+        
+        for (int i = 1; i <= 120; i++) {
+            model.addRow(new Object[]{" ", i, "Available/Unavailable"});
+        }
+
+        JTable staffChartTable = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(staffChartTable);
+
+        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.setSize(600, 400);
+        frame.setVisible(true);
+    }
+}
